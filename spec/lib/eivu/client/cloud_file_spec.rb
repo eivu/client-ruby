ede3c8f1abb9bd33cd9a1fab77a5a83404ce599f
Robert Jenkins <bobert@gmail.com>
2022-01-29 11:39:42 -0500

    change in whitespace and moved commented out code around

--- spec/lib/eivu/client/cloud_file_spec.rb
+++ spec/lib/eivu/client/cloud_file_spec.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 describe Eivu::Client::CloudFile do
-
-# reserve(md5:, bucket_id:, fullpath:, peepy: nil, nsfw: nil); end
-# {"id"=>285, "name"=>"Piano_brokencrash-Brandondorf-1164520478.mp3", "asset"=>"Piano_brokencrash-Brandondorf-1164520478.mp3", "md5"=>"A4FFA621BC8334B4C7F058161BDBABBF", "content_type"=>"audio/mpeg", "filesize"=>134899, "description"=>nil, "rating"=>nil, "nsfw"=>false, "peepy"=>false, "created_at"=>Thu, 14 May 2015 05:40:25.870345000 UTC +00:00, "updated_at"=>Thu, 14 May 2015 05:40:25.870345000 UTC +00:00, "folder_id"=>nil, "info_url"=>nil, "bucket_id"=>2, "duration"=>0, "settings"=>0, "ext_id"=>nil, "data_source_id"=>nil, "release_id"=>nil, "year"=>nil, "release_pos"=>nil, "user_id"=>nil, "num_plays"=>0, "state"=>"empty"}
-# md5= 'A4FFA621BC8334B4C7F058161BDBABBF'
-
-
   describe '.fetch' do
     subject(:instance) { described_class.fetch(md5) }
+
     context 'when md5 exists' do
       let(:md5) { 'A4FFA621BC8334B4C7F058161BDBABBF' }
-
 
       it 'returns a CloudFile instance' do
         expect(instance).to be_kind_of(described_class)
       end
     end
   end
+
+
+# reserve(md5:, bucket_id:, fullpath:, peepy: nil, nsfw: nil); end
+# {"id"=>285, "name"=>"Piano_brokencrash-Brandondorf-1164520478.mp3", "asset"=>"Piano_brokencrash-Brandondorf-1164520478.mp3", "md5"=>"A4FFA621BC8334B4C7F058161BDBABBF", "content_type"=>"audio/mpeg", "filesize"=>134899, "description"=>nil, "rating"=>nil, "nsfw"=>false, "peepy"=>false, "created_at"=>Thu, 14 May 2015 05:40:25.870345000 UTC +00:00, "updated_at"=>Thu, 14 May 2015 05:40:25.870345000 UTC +00:00, "folder_id"=>nil, "info_url"=>nil, "bucket_id"=>2, "duration"=>0, "settings"=>0, "ext_id"=>nil, "data_source_id"=>nil, "release_id"=>nil, "year"=>nil, "release_pos"=>nil, "user_id"=>nil, "num_plays"=>0, "state"=>"empty"}
+# md5= 'A4FFA621BC8334B4C7F058161BDBABBF'
+
   # describe '.reserve' do
   #   subject(:traversal) do
   #     described_class.traverse(path) { |x| x }
