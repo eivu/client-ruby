a9df8572ee07554d522a16bc4281abd49c70b33e
Robert Jenkins <bobert@gmail.com>
2022-04-18 15:59:21 -0400

    folder specs are now working

--- spec/lib/eivu/client/folder_spec.rb
+++ spec/lib/eivu/client/folder_spec.rb
@@ -2,28 +2,33 @@
 
 describe Eivu::Client::Folder do
   describe '.traverse' do
-    # subject(:traversal) do
-    #   described_class.traverse(path) { |x| x }
-    # end
+    subject(:traversal) do
+      described_class.traverse(path) { |x| x }
+    end
 
-    # context 'when path is a simple directory' do
-    #   let(:path) { 'lib/eivu/client' }
+    context 'when path is a simple directory' do
+      let(:path) { 'lib/eivu/client/errors' }
 
-    #   it 'returns a list of files' do
-    #     expect(traversal).to eq(
-    #       %w[lib/eivu/client/cloud_file.rb lib/eivu/client/folder.rb]
-    #     )
-    #   end
-    # end
+      it 'returns a list of files' do
+        expect(traversal).to eq(
+          %w[lib/eivu/client/errors/configuration.rb lib/eivu/client/errors/connection.rb]
+        )
+      end
+    end
 
-    # context 'when path has many subfolders and files' do
-    #   let(:path) { 'lib' }
+    context 'when path has many subfolders and files' do
+      let(:path) { 'lib/eivu' }
 
-    #   it 'returns a list of files' do
-    #     expect(traversal).to eq(
-    #       %w[lib/eivu/client/cloud_file.rb lib/eivu/client/folder.rb lib/eivu/client.rb lib/eivu.rb]
-    #     )
-    #   end
-    # end
+      it 'returns a list of files' do
+        expect(traversal).to contain_exactly(
+          'lib/eivu/client/cloud_file.rb',
+          'lib/eivu/client/configuration.rb',
+          'lib/eivu/client.rb',
+          'lib/eivu/client/folder.rb',
+          'lib/eivu/client/errors/configuration.rb',
+          'lib/eivu/client/errors/connection.rb'
+        )
+      end
+    end
   end
 end
