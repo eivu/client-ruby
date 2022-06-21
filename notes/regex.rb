328c89eb92859c16379b91533170af134e662c4c
Robert Jenkins <bobert@gmail.com>
2022-06-21 15:56:24 -0400

    adding notes on regex

---
+++ notes/regex.rb
@@ -0,0 +1,17 @@
+# regex: 
+# https://regex101.com/r/VydCXH/1\
+# https://stackoverflow.com/questions/18053865/regular-expression-for-double-parentheses
+
+
+# \(\((((?>[^\(\(\)\)]+)|(?R))*)\)\)
+
+# this ((is)) a ((very)) ((useful)) thing
+
+
+re = /\(\((((?>[^\(\(\)\)]+)|(?R))*)\)\)/i
+str = 'this ((is)) a ((very)) ((useful)) thing'
+
+# Print the match result
+str.scan(re) do |match|
+  puts match.to_s
+end
