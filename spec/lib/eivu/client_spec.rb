b2b197890a8fdb6618195bee88b0b2af36ab009e
Robert Jenkins <bobert@gmail.com>
2022-06-21 23:24:57 -0400

    refactoring specs

--- spec/lib/eivu/client_spec.rb
+++ spec/lib/eivu/client_spec.rb
@@ -68,40 +68,10 @@ describe Eivu::Client do
   describe '#upload_folder', vcr: true do
     subject(:result) { instance.upload_folder(path_to_folder:, peepy:, nsfw:) }
     let(:path_to_folder) { File.expand_path('../../fixtures/samples/audio/brothers_grimm', __dir__) }
-    let(:dummy_cloud_file) { instance_double(Eivu::Client::CloudFile) }
-    let(:cloud_file) { build :cloud_file }
     let(:peepy) { false }
     let(:nsfw) { false }
 
-    # context 'success, with mocks' do
-    #   before do
-    #     expect(Eivu::Client::CloudFile).to receive(:reserve).and_return(dummy_cloud_file)
-    #     expect(cloud_file).to receive(:transfer)
-    #     expect(cloud_file).to receive(:complete)
-    #     expect(cloud_file).to receive(:s3_folder).and_return('/path/to/s3/folder')
-    #     expect(s3_resource).to receive(:bucket).with(bucket_name).and_return(bucket)
-    #     expect(bucket).to receive(:object).with("#{cloud_file.s3_folder}/#{cloud_file.asset}").and_return(object)
-    #     expect(object).to receive(:upload_file).and_return(true)
-    #   end
-
-    #   let(:s3_resource) { double('Aws::S3::Resource') }
-    #   let(:bucket) { double('Aws::S3::Bucket') }
-    #   let(:object) { double('Aws::S3::Object') }
-
-    #   it 'writes the file to S3 and returns a collection of success statements' do
-    #     aggregate_failures do
-    #       expect(result[:failure].count).to eq(0)
-    #       expect(result[:success].count).to eq(5)
-    #       expect(result[:success].values).to all(eq('Upload successful'))
-    #     end
-    #   end
-    # end
-
-    context 'success, with mocks' do
-      # let(:s3_resource) { double('Aws::S3::Resource') }
-      # let(:bucket) { double('Aws::S3::Bucket') }
-      # let(:object) { double('Aws::S3::Object') }
-
+    context 'success' do
       it 'writes the file to S3 and returns a collection of success statements' do
         aggregate_failures do
           expect(result[:failure].count).to eq(0)
@@ -129,64 +99,4 @@ describe Eivu::Client do
       end
     end
   end
-
-
-
-#     context 'with mocks' do
-#       before do
-#         expect(s3_resource).to receive(:bucket).with(bucket_name).and_return(bucket)
-#         expect(bucket).to receive(:object).with("#{s3_folder}/#{filename}").and_return(object)
-#         expect(object).to receive(:upload_file).with(path_to_file, acl: 'public-read', content_type: kind_of(String),
-# metadata: {})
-#       end
-
-#       let(:s3_resource) { double('Aws::S3::Resource') }
-#       let(:bucket) { double('Aws::S3::Bucket') }
-#       let(:object) { double('Aws::S3::Object') }
-
-#       it 'writes the file to S3' do
-#         expects(write_to_s3).to be true
-#       end
-#     end
-
-
-
-# #     before do
-#       expect(Aws::S3::Resource).to receive(:new).and_return(resource)
-#       expect(CloudFile).to receive(:reserve).with(bucket_name: bucket_name, path_to_file: path_to_file, peepy: peepy, nsfw: nsfw).and_return(cloud_file)
-#       expect(resource).to receive_message_chain(:bucket, :object).with(bucket_name).with('PATH PRAM FOR CREATE OBJECT').and_return(object)
-#       # allow(bucket).to receive(:object).and_return(object)
-#       # allow(object).to receive(:upload_file)
-#
-#
-#
-# def create_object(path)
-# s3_resource.bucket(bucket_name).object(path)
-# end
-#
-# def write_to_s3(s3_resource, path_to_file)
-# # create file information for file on s3
-# store_dir = "#{s3_folder}/#{md5.scan(/.{2}|.+/).join('/')}"
-# filename  = File.basename(path_to_file)
-# mime      = MimeMagic.by_magic(file)
-# sanitized_filename = Eivu::Client::Utils.sanitize(filename)
-#
-# # upload the file to s3
-# s3_resource.bucket(bucket_name).object(path)
-# obj = create_object("#{store_dir}/#{sanitized_filename}")
-# obj.upload_file(path_to_file, acl: 'public-read', content_type: mime.type, metadata: {})
-#
-#     end
-#
-#     it 'does stuff' do
-#
-#     end
-#   end
-#       cloud_file = CloudFile.reserve(bucket_name: bucket_name, path_to_file: path_to_file, peepy: peepy, nsfw: nsfw)
-#       s3_resource = instantiate_s3_resource
-#       cloud_file.write_to_s3(s3_resource, path_to_file)
-#       cloud_file.transfer(path_to_file: path_to_file)
-#       cloud_file.complete(year: nil, rating: nil, release_pos: nil, metadata_list: {}, matched_recording: nil)
-#     end
-  # end
 end
