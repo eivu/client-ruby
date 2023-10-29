48f30770af4ec8121a64d0e74a2f899ff8fc836e
Robert Jenkins <bobert@gmail.com>
2023-10-28 23:38:38 -0400

    refactored Eivu::Client - removed write_to_s3 it was used for older version of ruby aws sdk - refactored validated_remote_md5! to call 2 other functions for ease of testing

--- lib/eivu/client.rb
+++ lib/eivu/client.rb
@@ -60,9 +60,6 @@ module Eivu
       remote_path_to_file = "#{cloud_file.s3_folder}/#{Utils.sanitize(filename)}"
 
       if cloud_file.reserved?
-        # unless write_to_s3(s3_resource:, s3_folder: cloud_file.s3_folder, path_to_file:)
-        #   raise Errors::CloudStorage::Connection, 'Failed to write to s3'
-        # end
         @logger.info '  Writing to S3'
         File.open(path_to_file, 'rb') do |file|
           s3_client.put_object(
@@ -118,56 +115,9 @@ module Eivu
       @configuration ||= self.class.configuration
     end
 
-    def write_to_s3(s3_resource:, s3_folder:, path_to_file:)
-      n = 0
-      temp_bytes = nil
-      temp_totals = nil
-      size = File.size(path_to_file)
-      bar = ProgressBar.create(:title => "Uploading action", :starting_at => 0, :total => size)
-      progress = Proc.new do |bytes, totals|
-        # binding.pry
-        # # binding.pry
-        # bar.progress += bytes.first
-        # binding.pry
-        # print "#{index} done. Progress: %.2f%" % (index.to_f / items * 100).round(2) + "\r" if (index % 10) == 0
-        percent = '%.2f' % (100.0 * bytes.sum / totals.sum)
-        suffix = percent.to_f < 100 ? "\r" : "\n"
-        # print "bytes #{bytes.first} | #{percent }% | #{totals.sum} | #{size}#{suffix}"
-        print "  Writing to s3: #{percent}%#{suffix}"
-        # logger.info "totals #{totals.first} | #{totals.last} | #{size}"
-
-        # temp_bytes = bytes
-        # temp_totals = totals
-        # logger.info bytes.map.with_index { |b, i| "Part #{i+1}: #{b} / #{totals[i]}"}.join(' ')# + "Total: #{100.0 * bytes.sum / totals.sum }%" }
-      end
-
-
-      # generate file information for file on s3
-      #
-      filename  = File.basename(path_to_file)
-      mime      = Utils.detect_mime(path_to_file)
-      sanitized_filename = Eivu::Client::Utils.sanitize(filename)
-      # upload the file to s3
-      #
-      # create object on s3
-      obj = s3_resource.bucket(configuration.bucket_name)
-                       .object("#{s3_folder}/#{sanitized_filename}")
-      # https://stackoverflow.com/questions/12085751/tracking-upload-progress-of-file-to-s3-using-ruby-aws-sdk/12147709#12147709
-      # https://stackoverflow.com/questions/50253068/upload-to-s3-with-progress-in-plain-ruby-script
-      # obj.write(:content_length => file.size) do |writable, n_bytes|
-      #   writable.write(file.read(n_bytes))
-      #   bar.progress += n_bytes
-      # end
-      obj.upload_file(path_to_file, acl: 'public-read', content_type: mime.type, metadata: {}, progress_callback: progress)
-    end
-
     def validated_remote_md5!(remote_path_to_file:, path_to_file:, md5:)
-      remote_md5 = s3_client.head_object({
-        bucket: configuration.bucket_name, 
-        key: remote_path_to_file
-      })&.etag&.gsub(/"/,'')
-
-      etag = `./bin/s3etag.sh "#{path_to_file}" 5`&.strip
+      remote_md5 = retrieve_remote_md5(remote_path_to_file)
+      etag       = generate_etag(path_to_file)
 
       unless [md5.downcase, etag].include?(remote_md5)
         raise Errors::CloudStorage::InvalidMd5, "Expected: #{md5.downcase}, Got: #{remote_md5}"
@@ -175,6 +125,20 @@ module Eivu
     end
 
     private
+
+    def retrieve_remote_md5(remote_path_to_file)
+      s3_client.head_object(
+        {
+          bucket: configuration.bucket_name,
+          key: remote_path_to_file
+        }
+      )&.etag&.gsub(/"/,'')
+    end
+
+    def generate_etag(path_to_file)
+      `./bin/s3etag.sh "#{path_to_file}" 5`&.strip
+    end
+
 
     def write_logs
       FileUtils.mkdir_p('logs')
